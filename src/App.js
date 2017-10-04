edfebe74b8f5b86268c7dd9cb2695a5c9d172e7f
Andrzej <andy-26@wp.pl>
2017-10-04 16:53:06 +0200

    shortHistory

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import { Button } from 'react-bootstrap';
 
 import './App.css';
 import Sidebar from "./Sidebar";
+import Diagrams from "./Diagrams";
+import ShortHistory from "./ShortHistory"
 
 class App extends Component {
   render() {
     return (
-
+<div>
      <Sidebar />
+     <Diagrams />
+      <ShortHistory/>
+</div>
+
+
     );
   }
 }
