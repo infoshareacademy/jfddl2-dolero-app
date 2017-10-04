458abe8738cff852b9fd869bae1b00000e64a2de
IT <piotr.siwek.90@gmail.com>
2017-10-04 16:26:54 +0200

    second commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
-class Sidebar extends Component.React {
+class Sidebar extends React.Component {
 
     state = {
-        userName : '',
+        userName: 'Andrzej',
         accountBalance: 0
     }
 
     render() {
-        return(
+        return (
             <div>
-                <h2>Witaj {this.state.userName}</h2>
-                <h3>{this.state.accountBalance}</h3>
+                <div>
+                    <h2>Witaj {this.state.userName}</h2>
+                    <h3>{this.state.accountBalance}</h3>
+                </div>
+                <form>
+                    <input type="number" placeholder="Wprowadź kwotę"/>
+                </form>
             </div>
         )
     }
