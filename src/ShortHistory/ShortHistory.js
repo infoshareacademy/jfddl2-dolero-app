30d4bd9a2c073f4d5cd30c44cc7abbd4d81c4b75
anyd007 <andy-26@wp.pl>
2017-10-05 20:51:24 +0200

    two tables

--- src/ShortHistory/ShortHistory.js
+++ src/ShortHistory/ShortHistory.js
@@ -7,36 +7,108 @@ Table
 class ShortHistory extends React.Component {
 
     render() {
-        return (
-            <Table striped bordered condensed hover>
+        return(
+            <div className="style">
+
+                <h2>Ostatnie wydatki</h2>
+
+            <Table>
                 <thead>
                 <tr>
                     <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
+                    <th>Zakup</th>
+                    <th>Kwota</th>
+                    <th>Data zakupu</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
                     <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td>Bułki</td>
+                    <td>5zł</td>
+                    <td>15.07.2107</td>
                 </tr>
                 <tr>
                     <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>Kasza</td>
+                    <td>3zł</td>
+                    <td>15.07.2107</td>
                 </tr>
                 <tr>
                     <td>3</td>
-                    <td colSpan="2">Larry the Bird</td>
-                    <td>@twitter</td>
+                    <td>Ser biały</td>
+                    <td>3zł</td>
+                    <td>15.07.2107</td>
                 </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Tuńczyk</td>
+                        <td>7zł</td>
+                        <td>15.07.2107</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Ryż</td>
+                        <td>3zł</td>
+                        <td>15.07.2107</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Jogurt naturalny</td>
+                        <td>4zł</td>
+                        <td>15.07.2107</td>
+                    </tr>
                 </tbody>
             </Table>
+
+
+
+
+    <h2>Ostatnie wpływy</h2>
+    <Table>
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Wpłata tytułem</th>
+            <th>Kwota wpłaty</th>
+            <th>Data wpłaty</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>Czesne od Piotrka</td>
+            <td>5000zł</td>
+            <td>05.10.2107</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Czesne od Jarka</td>
+            <td>5000zł</td>
+            <td>05.10.2107</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Czesne od Moniki</td>
+            <td>5000zł</td>
+            <td>05.10.2107</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Zwrot infoShare</td>
+            <td>7000zł</td>
+            <td>05.10.2107</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Dodatek</td>
+            <td>3000zł</td>
+            <td>05.10.2107</td>
+        </tr>
+        </tbody>
+    </Table>
+
+            </div>
         )
     }
 }
