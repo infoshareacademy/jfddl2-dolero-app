d0c67a8f6aefe50c7482edb662cd187c6b8c9b88
jarek-mazur <jarekmzr@gmail.com>
2017-10-05 10:26:37 +0200

    menu added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import {Button} from 'react-bootstrap';
-
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom'
+import {
+    Grid
+} from 'react-bootstrap'
 import './App.css';
 import Sidebar from "./Sidebar";
 // import Diagrams from "./Diagrams";
-import History from "./History";
+// import Diagrams from "./ShortHistory";
+
+import History from "./History/History";
+import MainMenu from './MainMenu'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                {/*<Sidebar/>*/}
-                <History/>
-                {/*<Diagrams/>*/}
-            </div>
+            <Router>
+                <Grid>
+
+                    <Sidebar/>
+                    <MainMenu/>
+                    <Route path='/history' component={History}/>
+                    {/*<Route path='/diagrams' component={Diagrams}/>*/}
+                    {/*<Route path='/shorthistory' component={ShortHistory}/>*/}
+
+                </Grid>
+            </Router>
         );
     }
 }
