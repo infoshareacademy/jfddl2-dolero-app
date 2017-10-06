5f4193823f0118218cbf68cc87e5ab31601e97f1
Andrzej <andy-26@wp.pl>
2017-10-06 16:38:07 +0200

    add table with object

--- src/ShortHistory/ShortHistory.js
+++ src/ShortHistory/ShortHistory.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import './ShortHistory.css';
+import { BootstrapTable, TableHeaderColumn, priceFormatter} from 'react-bootstrap-table';
 import {
 Table
 } from 'react-bootstrap'
+
+var products = [{
+    id: 1,
+    name: "Item name 1",
+    price: 100
+}, {
+    id: 2,
+    name: "Item name 2",
+    price: 100
+}, {
+    id: 3,
+    name: "Item name 3",
+    price: 100
+}, {
+    id: 4,
+    name: "Item name 4",
+    price: 100
+}, {
+    id: 5,
+    name: "Item name 5",
+    price: 100
+}, {
+    id: 6,
+    name: "Item name 6",
+    price: 100
+}, {
+    id: 7,
+    name: "Item name 7",
+    price: 100
+}];
+
 
 class ShortHistory extends React.Component {
 
@@ -10,104 +42,22 @@ class ShortHistory extends React.Component {
         return(
             <div className="style">
 
-                <h2>Ostatnie wydatki</h2>
-
-            <Table>
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Zakup</th>
-                    <th>Kwota</th>
-                    <th>Data zakupu</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Bułki</td>
-                    <td>5zł</td>
-                    <td>15.07.2107</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Kasza</td>
-                    <td>3zł</td>
-                    <td>15.07.2107</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Ser biały</td>
-                    <td>3zł</td>
-                    <td>15.07.2107</td>
-                </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Tuńczyk</td>
-                        <td>7zł</td>
-                        <td>15.07.2107</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>Ryż</td>
-                        <td>3zł</td>
-                        <td>15.07.2107</td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>Jogurt naturalny</td>
-                        <td>4zł</td>
-                        <td>15.07.2107</td>
-                    </tr>
-                </tbody>
-            </Table>
+                <h2>Ostatnie Przychody</h2>
 
 
+                <BootstrapTable data={products} striped={true} hover={true}>
+                    <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Income</TableHeaderColumn>
+                    <TableHeaderColumn dataField="name" dataSort={true} dataAlign="center">Income From</TableHeaderColumn>
+                    <TableHeaderColumn dataField="price" dataFormat={priceFormatter} dataAlign="center">Product Price</TableHeaderColumn>
+                </BootstrapTable>
 
+                <h2>Ostatnie Wydatki</h2>
 
-    <h2>Ostatnie wpływy</h2>
-    <Table>
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>Wpłata tytułem</th>
-            <th>Kwota wpłaty</th>
-            <th>Data wpłaty</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>1</td>
-            <td>Czesne od Piotrka</td>
-            <td>5000zł</td>
-            <td>05.10.2107</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Czesne od Jarka</td>
-            <td>5000zł</td>
-            <td>05.10.2107</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Czesne od Moniki</td>
-            <td>5000zł</td>
-            <td>05.10.2107</td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>Zwrot infoShare</td>
-            <td>7000zł</td>
-            <td>05.10.2107</td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>Dodatek</td>
-            <td>3000zł</td>
-            <td>05.10.2107</td>
-        </tr>
-        </tbody>
-    </Table>
-
+                <BootstrapTable data={products} striped={true} hover={true}>
+                    <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Outcome</TableHeaderColumn>
+                    <TableHeaderColumn dataField="name" dataSort={true} dataAlign="center">Product Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField="price" dataFormat={priceFormatter} dataAlign="center">Product Price</TableHeaderColumn>
+                </BootstrapTable>
             </div>
         )
     }
