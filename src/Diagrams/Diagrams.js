8c70dca2b03c52b1925e9c322cab54a770447d4b
mopacewicz <31477168+mopacewicz@users.noreply.github.com>
2017-10-09 15:23:35 +0200

    add marginRight

--- src/Diagrams/Diagrams.js
+++ src/Diagrams/Diagrams.js
@@ -3,7 +3,8 @@ import {Panel} from 'react-bootstrap'
 import {Grid, Row, Col} from 'react-bootstrap'
 import {DateRangePicker} from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {PieChart, Pie } from 'recharts'
 
 const data = [
     {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
@@ -14,6 +15,21 @@ const data = [
     {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
     {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
 ]
+
+const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+    {name: 'Group C', value: 300}, {name: 'Group D', value: 200}]
+
+const data02 = [{name: 'A1', value: 100},
+    {name: 'A2', value: 300},
+    {name: 'B1', value: 100},
+    {name: 'B2', value: 80},
+    {name: 'B3', value: 40},
+    {name: 'B4', value: 30},
+    {name: 'B5', value: 50},
+    {name: 'C1', value: 100},
+    {name: 'C2', value: 200},
+    {name: 'D1', value: 150},
+    {name: 'D2', value: 50}]
 
 const transactions = [
     {amount: 300, category: "food", date: new Date(2017, 9, 9)},
@@ -72,12 +88,13 @@ class Diagrams extends React.Component {
         totalIncome: 0,
     };
 
+
     render() {
         return (
             <div style={{marginLeft: 15 + "px"}}>
                 <h1>Overview/charts</h1>
                 <Grid>
-                    <Row className="show-grid">
+                    <Row className="show-grid" style={{marginRight: -90 + "px"}} >
                         <Col md={2} mdOffset={6}>
                             <DateRangePicker
                                 startDate={this.state.startDate}
@@ -97,7 +114,7 @@ class Diagrams extends React.Component {
                         </Col>
                     </Row>
 
-                    <Row className="show-grid">
+                    <Row className="show-grid"style={{marginRight: -90 + "px"}}>
                         <Col md={2}>
                             <Panel header="Current balance">
                                 {this.state.currentBalance}
@@ -136,6 +153,11 @@ class Diagrams extends React.Component {
                     <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
                     <Line type="monotone" dataKey="uv" stroke="#82ca9d"/>
                 </LineChart>
+                <PieChart width={800} height={400}>
+                    <Pie data={data01} cx={200} cy={200} outerRadius={60} fill="#8884d8"/>
+                    <Pie data={data02} cx={200} cy={200} innerRadius={70} outerRadius={90} fill="#82ca9d" label/>
+                </PieChart>
+
             </div>
         )
     }
