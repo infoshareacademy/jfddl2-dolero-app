e0a6029cbb472acdda7b60b6d4acefbd53c7985b
Andrzej <andy-26@wp.pl>
2017-10-24 15:46:48 +0200

    add

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,23 +1,42 @@
 import React from 'react'
 import firebase from "firebase"
-import {Button} from 'react-bootstrap'
+import {Button,
+        FormGroup,
+        FormControl,
+        HelpBlock,
+        ControlLabel,
+        InputGroup,
+        Form,
+        Col
+} from 'react-bootstrap'
+
+function FieldGroup({ id, label, help, ...props }) {
+    return (
+        <FormGroup controlId={id}>
+            <ControlLabel>{label}</ControlLabel>
+            <FormControl {...props} />
+            {help && <HelpBlock>{help}</HelpBlock>}
+        </FormGroup>
+    );
+}
+
+
 
 class Settings extends React.Component {
 
-
+         print_current_page = () => window.print()
             // reset hasła
                 HunddleResetPass=() => {
                     const email = firebase.auth().currentUser.email
                     firebase.auth().sendPasswordResetEmail(email).then(
-                        () => alert('Sprawdź e-maila i kilknij w link')
+                        () => alert('Sprawdź swoją pocztę, został wysłany e-mail z linkiem do ustawienia nowego hasła')
                     )
                 }
 
-
 render(){
+
     return(
         <div>
-            {/*reset hasła*/}
 
             <form>
                 <input
@@ -32,7 +51,98 @@ render(){
                     Dodaj zdjęcie profilowe
                 </Button>
             </form>
+
             <Button bsSize="large" bsStyle="danger" onClick={this.HunddleResetPass}>Nadaj nowe hasło</Button>
+
+<Form>
+    <h2>Dane do faktury</h2>
+    <Col sm={6}>
+    <FormGroup>
+    <InputGroup>
+        <InputGroup.Addon>Imię</InputGroup.Addon>
+    <FieldGroup
+        id="formControlsText"
+        type="text"
+        label=""
+        placeholder="Wprowadz dane"
+    />
+    </InputGroup>
+    </FormGroup>
+    </Col>
+
+    <Col sm={6}>
+    <FormGroup>
+    <InputGroup>
+        <InputGroup.Addon>Nazwisko</InputGroup.Addon>
+    <FieldGroup
+        id="formControlsText"
+        type="text"
+        label=""
+        placeholder="Wprowadź dane"
+    />
+    </InputGroup>
+    </FormGroup>
+    </Col>
+
+    <Col sm={8}>
+<FormGroup>
+    <InputGroup>
+    <InputGroup.Addon>Nazwa firmy</InputGroup.Addon>
+    <FieldGroup
+        id="formControlsText"
+        type="text"
+        label=""
+        placeholder="Wprowadź dane"
+    />
+    </InputGroup>
+</FormGroup>
+    </Col>
+
+    <Col sm={4}>
+<FormGroup>
+    <InputGroup>
+        <InputGroup.Addon>NIP</InputGroup.Addon>
+        <FieldGroup
+        id="formControlsText"
+        type="text"
+        label=""
+        placeholder="Wprowadź dane"
+        />
+    </InputGroup>
+</FormGroup>
+    </Col>
+
+    <Col sm={6}>
+    <FormGroup>
+            <InputGroup>
+                <InputGroup.Addon>Kwota</InputGroup.Addon>
+                <FieldGroup
+                    id="formControlsText"
+                    type="number"
+                    label=""
+                    placeholder="Podaj wartość"
+                />
+            </InputGroup>
+    </FormGroup>
+    </Col>
+
+    <Col sm={6}>
+    <FormGroup>
+            <InputGroup>
+                <InputGroup.Addon>Adres</InputGroup.Addon>
+        <FormControl componentClass="textarea" placeholder="podaj dane adresowe" controlId="formControlsTextarea"/>
+            </InputGroup>
+    </FormGroup>
+    </Col>
+
+    <Button onClick={this.print_current_page}
+            type="submit"
+            bsStyle="warning"
+    >
+        Zatwierdź
+    </Button>
+</Form>
+
         </div>
 
 
